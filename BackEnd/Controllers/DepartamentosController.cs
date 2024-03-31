023c1c0017a353347871b290721b15f7ea7ada2d
ZaqueuCavalcante <zaqueudovale@gmail.com>
2024-03-31 08:12:24 -0300

    Issue #18: Create departamento

--- BackEnd/Controllers/DepartamentosController.cs
+++ BackEnd/Controllers/DepartamentosController.cs
@@ -1,5 +1,6 @@
-using Mech.Code.Dtos;
+using Mech.Domain;
 using Mech.Database;
+using Mech.Code.Dtos;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,11 +10,31 @@ namespace Mech.Code.Controllers;
 public class DepartamentosController(MechDbContext ctx) : ControllerBase
 {
     /// <summary>
+    /// Cria um novo departamento.
+    /// </summary>
+    [HttpPost("")]
+    [Produces("application/json"), Consumes("application/json")]
+    [ProducesResponseType(typeof(DepartamentoOut), 200)]
+    public async Task<IActionResult> Create([FromBody] DepartamentoIn data)
+    {
+        var departamento = new Departamento
+        {
+            Nome = data.Nome,
+            Descricao = data.Descricao
+        };
+
+        ctx.Add(departamento);
+        await ctx.SaveChangesAsync();
+
+        return Ok(new DepartamentoOut { Id = departamento.Id, Nome = departamento.Nome, Descricao = departamento.Descricao });
+    }
+
+    /// <summary>
     /// Retorna todos os departamentos.
     /// </summary>
     [HttpGet("")]
     [Produces("application/json")]
-    [ProducesResponseType(typeof(DepartamentoOut), 200)]
+    [ProducesResponseType(typeof(List<DepartamentoOut>), 200)]
     public async Task<IActionResult> GetAll()
     {
         var departamentos = await ctx.Departamentos.ToListAsync();
